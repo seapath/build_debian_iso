1fa8abcbaeaf2267c7dd7b74b010499cd1e060ff
watare <aurelien.watare@gmail.com>
2022-05-25 17:10:23 +0200

    build.sh:bug fix
    Signed-off-by: watare <aurelien.watare@gmail.com>

--- build_iso.sh
+++ build_iso.sh
@@ -2,12 +2,12 @@
 
 wd=`dirname $0`
 #docker-compose -f $wd/docker-compose.yml run --rm fai-setup bash -c "rm -rf /ext/*"
-docker volume rm fai_docker_ext 2>/dev/null
+docker volume rm  build_debian_iso_ext2>/dev/null
 docker-compose -f $wd/docker-compose.yml run --rm fai-setup fai-setup -v -e
 docker-compose -f $wd/docker-compose.yml run --rm fai-setup fai-mirror -c DEBIAN,SEAPATH_LVM,FAIBASE,DEMO,SEAPATH,SEAPATH_NOLVM,GRUB_EFI /ext/mirror
 docker-compose -f $wd/docker-compose.yml run --rm fai-cd fai-cd -f -m /ext/mirror /ext/seapath.iso
 docker-compose -f $wd/docker-compose.yml up --no-start fai-setup
 docker cp fai-setup:/ext/seapath.iso .
 docker-compose -f $wd/docker-compose.yml down
-docker volume rm fai_docker_ext
+docker volume rm build_debian_iso_ext 
 
