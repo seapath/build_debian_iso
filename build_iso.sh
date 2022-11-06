af5609125404d0f0f59c7b08a0cd6e7899ae384b
Florent CARLI <florent.carli@rte-france.com>
2022-11-06 15:47:21 +0100

    Include docker image "php:apache" in iso
    Signed-off-by: Florent CARLI <florent.carli@rte-france.com>

--- build_iso.sh
+++ build_iso.sh
@@ -4,14 +4,27 @@ wd=`dirname $0`
 #docker-compose -f $wd/docker-compose.yml run --rm fai-setup bash -c "rm -rf /ext/*"
 
 # removing the volume in case it exists from a precedent build operation
-docker volume rm  build_debian_iso_ext 2>/dev/null
+docker rm -f fai-setup 2>/dev/null
+docker volume rm build_debian_iso_ext 2>/dev/null
 
 # creating the NFSROOT
 docker-compose -f $wd/docker-compose.yml run --rm fai-setup bash -c "fai-setup -v -e; rm -f /ext/srv/fai/config/class/*.profile"
 
-# Addding the SEAPATH workspace
+# Starting the container to add stuff in it
 docker-compose -f $wd/docker-compose.yml up --no-start fai-setup
+
+# Adding the SEAPATH workspace
 docker cp $wd/srv_fai_config/. fai-setup:/ext/srv/fai/config/
+# Adding the php:apache docker image
+docker pull php:apache
+mkdir -p /tmp/php_image/opt/php_apache.tgz
+docker save php:apache | gzip > /tmp/php_image/opt/php_apache.tgz/SEAPATH.tgz
+mv /tmp/php_image/opt/php_apache.tgz/SEAPATH.tgz /tmp/php_image/opt/php_apache.tgz/SEAPATH
+echo docker cp /tmp/php_image/. fai-setup:/ext/src/fai/files/
+docker cp /tmp/php_image/. fai-setup:/ext/srv/fai/config/files/
+rm -rf /tmp/php_image/
+
+# Stopping the container after having added stuff in it
 docker-compose -f $wd/docker-compose.yml down
 
 # Creating the mirror
