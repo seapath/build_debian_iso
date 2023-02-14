b6d70260529539d8975663a2fa8e1c8854df7b8f
Enguerrand de Ribaucourt <enguerrand.de-ribaucourt@savoirfairelinux.com>
2023-02-14 14:58:26 +0100

    build_iso.sh: catch fai errors
    Some FAI silently failed which was unstable.
    Add `set -e` to correctly abort on errors and fix the errors that were
    discovered.
    
    SFL: #8685
    Change-Id: I83bf898e3d8b6b9809d92f18dfc535a11d7bc8c5
    Signed-off-by: Enguerrand de Ribaucourt <enguerrand.de-ribaucourt@savoirfairelinux.com>

--- build_iso.sh
+++ build_iso.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 wd=$(dirname $0)
-#docker-compose -f $wd/docker-compose.yml run --rm fai-setup bash -c "rm -rf /ext/*"
 
+rm $wd/seapath.iso
 # removing the volume in case it exists from a precedent build operation
 docker rm -f fai-setup 2>/dev/null
 docker volume rm build_debian_iso_ext 2>/dev/null
 
-# creating the NFSROOT
-docker-compose -f $wd/docker-compose.yml run --rm fai-setup bash -c "fai-setup -v -e; rm -f /ext/srv/fai/config/class/*.profile"
+set -ex
+
+# Creating the NFSROOT
+# Removing *.profile makes our seapath.profile the default
+docker-compose -f $wd/docker-compose.yml run --rm fai-setup bash -c "fai-setup -v -e -f && rm -f /ext/srv/fai/config/class/*.profile"
 
 # Starting the container to add stuff in it
 docker-compose -f $wd/docker-compose.yml up --no-start fai-setup
@@ -34,7 +37,7 @@ docker-compose -f $wd/docker-compose.yml run --rm fai-cd fai-cd -f -m /ext/mirro
 
 # Retrieving the ISO from the volume
 docker-compose -f $wd/docker-compose.yml up --no-start fai-setup
-docker cp fai-setup:/ext/seapath.iso .
+docker cp fai-setup:/ext/seapath.iso $wd/
 docker-compose -f $wd/docker-compose.yml down
 
 # Removing the volume
