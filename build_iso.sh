79effc09e6dad7c1fecf8f315608958f5bfa806e
Florent CARLI <florent.carli@rte-france.com>
2023-08-16 15:36:43 +0200

    debian12 upgrade
    This commit contains all the modifications required for a update to debian 12:
    - change of all the bullseye references to bookworm
    - solves a bug in FAI while it's dealt with upstream
    - adapt the list of packages for bookworm
    - also adds a RAIDDEMO flavor for dev and test purposes
    
    Signed-off-by: Florent CARLI <florent.carli@rte-france.com>

--- build_iso.sh
+++ build_iso.sh
@@ -13,7 +13,8 @@ set -ex
 # Creating the NFSROOT
 # Removing *.profile since we don't use them
 # Removing 50-host-classes to prevent DEMO and FAIBASE to be added to the list of classes
-# Adding the Bullseye basefiles to that we deploy a Debian v11 distro
+# Adding the Bookworm basefiles to that we deploy a Debian v12 distro
+# Patches /sbin/install_packages (bug in the process of being corrected upstream)
 docker-compose -f $wd/docker-compose.yml run --rm fai-setup bash -c "\
     echo \"fai-setup -v -e -f \" && \
     fai-setup -v -e -f && \
@@ -21,8 +22,11 @@ docker-compose -f $wd/docker-compose.yml run --rm fai-setup bash -c "\
     rm -f /ext/srv/fai/config/class/50-host-classes && \
     echo \"rm -f /ext/srv/fai/config/class/*.profile\" && \
     rm -f /ext/srv/fai/config/class/*.profile && \
-    echo \"wget -O /ext/srv/fai/config/basefiles/BULLSEYE64.tar.xz https://fai-project.org/download/basefiles/BULLSEYE64.tar.xz\" && \
-    wget -O /ext/srv/fai/config/basefiles/BULLSEYE64.tar.xz https://fai-project.org/download/basefiles/BULLSEYE64.tar.xz"
+    echo \"SED\" && \
+    sed -i -e \"s|-f \\\"\$FAI_ROOT/var/cache/apt/pkgcache\.bin|-d \\\"\$FAI_ROOT/var/lib/apt/lists|\" /ext/nfsroot/sbin/install_packages && \
+    sed -i -e \"s/ --allow-change-held-packages//\" /ext/nfsroot/sbin/install_packages && \
+    echo \"wget -O /ext/srv/fai/config/basefiles/BOOKWORM64.tar.xz https://fai-project.org/download/basefiles/BOOKWORM64.tar.xz\" && \
+    wget -O /ext/srv/fai/config/basefiles/BOOKWORM64.tar.xz https://fai-project.org/download/basefiles/BOOKWORM64.tar.xz"
 
 # Starting the container to add stuff in it
 docker-compose -f $wd/docker-compose.yml up --no-start fai-setup
