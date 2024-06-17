cfae13443614e9d233d3d0032454cfb139207631
Florent CARLI <florent.carli@rte-france.com>
2024-06-17 11:29:34 +0200

    fix shellcheck build_qcow2.sh
    Signed-off-by: Florent CARLI <florent.carli@rte-france.com>

--- build_qcow2.sh
+++ build_qcow2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wd=$(dirname $0)
+wd=$(dirname "$0")
 output_dir=.
 
 # test if "docker compose" works
@@ -21,31 +21,31 @@ docker volume rm build_debian_iso_ext 2>/dev/null
 set -ex
 
 # Create the default config space
-$COMPOSECMD -f $wd/docker-compose.yml run --rm fai-setup \
+$COMPOSECMD -f "$wd"/docker-compose.yml run --rm fai-setup \
     fai-mk-configspace
 
 # Starting the container to add seapath stuff in the config space
-$COMPOSECMD -f $wd/docker-compose.yml up --no-start fai-setup
+$COMPOSECMD -f "$wd"/docker-compose.yml up --no-start fai-setup
 
 # Adding the SEAPATH config
-docker cp $wd/srv_fai_config/. fai-setup:ext/srv/fai/config/
+docker cp "$wd"/srv_fai_config/. fai-setup:ext/srv/fai/config/
 
 # Stopping the container after having added stuff in it
-$COMPOSECMD -f $wd/docker-compose.yml down
+$COMPOSECMD -f "$wd"/docker-compose.yml down
 
 # Creating the VM
 # patches /sbin/install_packages (bug in the process of being corrected upstream)
 CLASSES="DEBIAN,FAIBASE,FRENCH,BOOKWORM64,SEAPATH_COMMON,SEAPATH_VM,GRUB_EFI,LAST"
-$COMPOSECMD -f $wd/docker-compose.yml run fai-cd bash -c "\
+$COMPOSECMD -f "$wd"/docker-compose.yml run fai-cd bash -c "\
   sed -i -e \"s|-f \\\"\\\$FAI_ROOT/usr/sbin/apt-cache|-f \\\"\\\$FAI_ROOT/usr/bin/apt-cache|\" /sbin/install_packages && \
   sed -i -e \"s/ --allow-change-held-packages//\" /sbin/install_packages && \
   sed -i -e \"s/-c -o compression_type=zstd qcow2/qcow2/\" /usr/sbin/fai-diskimage && \
   fai-diskimage -vu seapath-vm -S10G -c$CLASSES -s /ext/srv/fai/config /ext/seapath-vm.qcow2"
 
 # Retrieving the ISO from the volume
-$COMPOSECMD -f $wd/docker-compose.yml up --no-start fai-setup
+$COMPOSECMD -f "$wd"/docker-compose.yml up --no-start fai-setup
 docker cp fai-setup:/ext/seapath-vm.qcow2 $output_dir/
-$COMPOSECMD -f $wd/docker-compose.yml down --remove-orphans
+$COMPOSECMD -f "$wd"/docker-compose.yml down --remove-orphans
 
 # Removing the volume
 docker volume rm build_debian_iso_ext
