711a66e6dd8df93e31559e0cd21935f7628ea204
Florent CARLI <florent.carli@rte-france.com>
2026-01-28 16:15:04 +0100

    autodetect podman or docker
    We previously switch the build_iso/build_qcow2 from docker to podman.
    This commits autodetects podman, and fails back to docker if podman is not installed.
    
    Signed-off-by: Florent CARLI <florent.carli@rte-france.com>

--- build_qcow2.sh
+++ build_qcow2.sh
@@ -47,13 +47,34 @@ if [ $# -gt 0 ]; then
     echo "Additional arguments: $*"
 fi
 
-COMPOSECMD=(sudo podman-compose)
-echo "We are going to use" "${COMPOSECMD[@]}"
+# Check for container tools and set commands accordingly
+if command -v podman-compose &> /dev/null && command -v podman &> /dev/null; then
+    COMPOSECMD=(sudo podman-compose)
+    CONTAINER_ENGINE=(podman)
+    echo "We are going to use ${COMPOSECMD[@]}"
+elif command -v docker-compose &> /dev/null && command -v docker &> /dev/null; then
+    COMPOSECMD=(docker-compose)
+    CONTAINER_ENGINE=(docker)
+    echo "We are going to use ${COMPOSECMD[@]}"
+elif command -v docker &> /dev/null && docker compose version &> /dev/null; then
+    # Check for newer 'docker compose' (plugin) syntax
+    COMPOSECMD=(docker compose)
+    CONTAINER_ENGINE=(docker)
+    echo "We are going to use ${COMPOSECMD[@]}"
+else
+    echo "Error: Neither podman-compose/podman nor docker-compose/docker found in PATH" >&2
+    echo "Please install one of the following:" >&2
+    echo "  - podman and podman-compose" >&2
+    echo "  - docker and docker-compose" >&2
+    exit 1
+fi
+
+echo "We are going to use" "${CONTAINER_ENGINE[@]}" and "${COMPOSECMD[@]}"
 
 rm -f $output_dir/seapath-vm.qcow2
 # removing the volume in case it exists from a precedent build operation
-sudo podman rm -f fai-setup 2>/dev/null
-sudo podman volume rm build_debian_iso_ext 2>/dev/null
+sudo "${CONTAINER_ENGINE[@]}" rm -f fai-setup 2>/dev/null
+sudo "${CONTAINER_ENGINE[@]}" volume rm build_debian_iso_ext 2>/dev/null
 
 set -ex
 
@@ -69,7 +90,7 @@ cp -r "$wd/usercustomization/"* "$wd/build_tmp"
 "${COMPOSECMD[@]}" -f "$(realpath $wd/docker-compose.yml)" up --no-start fai-setup
 
 # Adding the SEAPATH config
-sudo podman cp "$wd"/build_tmp/. fai-setup:ext/srv/fai/config/
+sudo "${CONTAINER_ENGINE[@]}" cp "$wd"/build_tmp/. fai-setup:ext/srv/fai/config/
 
 # Stopping the container after having added stuff in it
 "${COMPOSECMD[@]}" -f "$(realpath $wd/docker-compose.yml)" down
@@ -93,10 +114,10 @@ CLASSES="DEBIAN,FAIBASE,FRENCH,TRIXIE64,SEAPATH_COMMON,GRUB_EFI,SEAPATH_RAW,${se
 
 # Retrieving the ISO from the volume
 "${COMPOSECMD[@]}" -f "$(realpath $wd/docker-compose.yml)" up --no-start fai-setup
-echo sudo podman cp fai-setup:/ext/seapath-vm.qcow2 $output_dir/
-sudo podman cp fai-setup:/ext/seapath-vm.qcow2 $output_dir/
+echo sudo "${CONTAINER_ENGINE[@]}" cp fai-setup:/ext/seapath-vm.qcow2 $output_dir/
+sudo "${CONTAINER_ENGINE[@]}" cp fai-setup:/ext/seapath-vm.qcow2 $output_dir/
 "${COMPOSECMD[@]}" -f "$(realpath $wd/docker-compose.yml)" down --remove-orphans
 
 # Removing the volume
-sudo podman volume rm build_debian_iso_ext
+sudo "${CONTAINER_ENGINE[@]}" volume rm build_debian_iso_ext
 rm -rf "$wd"/build_tmp/*
